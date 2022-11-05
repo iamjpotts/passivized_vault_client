622c864201c19dfcf154f3d518de92d367314c13
iamjpotts <8704475+iamjpotts@users.noreply.github.com>
2022-11-05 12:47:07 -0500

    Refactor shared configuration (#10)

---
+++ examples/example_utils/hcl.rs
@@ -0,0 +1,43 @@
+use std::borrow::Borrow;
+use std::fs::File;
+use std::path::Path;
+use tempfile::NamedTempFile;
+
+pub const VAULT_CONFIG_PATH: &str = "/vault/config/config.hcl";
+
+pub const VAULT_CONFIG_HCL: &str = "storage \"file\" {
+    path    = \"/vault/file\"
+}
+
+listener \"tcp\" {
+   address = \"0.0.0.0:8200\"
+   tls_disable = true
+}
+
+ui = false";
+
+pub fn create_vault_config_file() -> Result<NamedTempFile, std::io::Error> {
+    create_vault_config_file_with_content(VAULT_CONFIG_HCL)
+}
+
+pub fn create_vault_config_file_with_content<C>(content: C) -> Result<NamedTempFile, std::io::Error>
+where
+    C: Borrow<str>
+{
+    use std::io::Write;
+
+    let mut ntf = NamedTempFile::new()?;
+    write!(ntf, "{}", content.borrow())?;
+
+    set_vault_config_permissions(ntf.path())?;
+
+    Ok(ntf)
+}
+
+pub fn set_vault_config_permissions(path: &Path) -> Result<(), std::io::Error> {
+    use std::os::unix::fs::PermissionsExt;
+
+    let f = File::open(path)?;
+    f.set_permissions(PermissionsExt::from_mode(0o644))
+}
+
--- examples/example_utils/lib.rs
+++ examples/example_utils/lib.rs
@@ -1,5 +1,9 @@
 #![allow(dead_code)]  // Each example compiles separately; not all examples use all example utilities.
 
 pub mod errors;
+
+#[cfg(not(windows))]
+pub mod hcl;
+
 pub mod images;
 pub mod timestamps;
